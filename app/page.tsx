1047125f116abbe24c321bcd1be4420f707bccd3
Yashwanth-Kumar24 <yashwanth.jannaikode@gmail.com>
2025-12-30 12:19:57 -0500

    route updated

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace("/companies");
+    router.replace("/today");
   }, [router]);
 
   return null;
